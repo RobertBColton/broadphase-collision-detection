10e99b89400d2f1dadb0c92dba691759d3e5904f
unknown <robertbcolton@gmail.com>
2020-03-03 22:54:40 -0500

    Add launcher dialog for additional broadphases

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include "MainWindow.hpp"
-#include <QApplication>
+#include <QtWidgets>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	MainWindow w;
-	w.show();
+	QDialog launcher(nullptr);
+	launcher.setWindowFlags(launcher.windowFlags() & ~Qt::WindowContextHelpButtonHint);
+	launcher.setWindowTitle("Broadphase Collision Detection");
+
+	QVBoxLayout *vbl = new QVBoxLayout();
+	QPushButton *bmButton = new QPushButton("Benchmark");
+	vbl->addWidget(bmButton);
+
+	QList<QString> bpis = {"K-D Tree", "K-D-B Tree", "PR Quadtree"};
+	foreach (auto bp, bpis) {
+		QPushButton *bpButton = new QPushButton(bp);
+		vbl->addWidget(bpButton);
+	}
+
+	launcher.setLayout(vbl);
+	launcher.resize(600,400);
+	launcher.show();
+
+	//MainWindow w;
+	//w.show();
 
 	return a.exec();
 }
