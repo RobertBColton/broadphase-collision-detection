1799f3a3bcbc3e7a7fd1909c39aec6004c44bd53
unknown <robertbcolton@gmail.com>
2020-03-23 18:09:37 -0400

    Add explanation for why prune sweep is pointless.

--- PruneSweep.hpp
+++ PruneSweep.hpp
@@ -11,6 +11,23 @@
 
 #include "Broadphase.hpp"
 
+/*
+ * Prune Sweep Sucks
+ *
+ * I spent some time fiddling with this algorithm before I realized its absolutely pointless nature
+ * akin to that of the kd tree or any of the other dynamic tree solutions to the problem of broadphase.
+ * Starting out with the use of a vector, your update complexity can not beat O(n log n) as it
+ * essentially has to resort the proxies. The time could be improved using a linked list allowing
+ * constant insertion and removal to move the proxies in the list during sort, but then binary search
+ * becomes impossible and degenerates to O(n). A skip list is thus the next logical step but is the
+ * same as a binary space partition at that point, which I also ruled out as useless due to its
+ * similarity to the quadtree!
+ *
+ * Summary: Prune Sweep -> A poor man's binary space partition. Consider this class simply the base time
+ * for comparison in the benchmark table.
+ *
+ */
+
 class PruneSweep : public Broadphase {
 
 public:
